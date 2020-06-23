10a2d384635c458dd1c0187e3056e76e0a18306d
Thijmen de Valk <thijmen@wingpictures.nl>
2020-06-24 01:19:54 +0200

    Let's finish this up

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,14 +15,14 @@ const startingPosition = [-80.5648, 28.4734]; // Cape Canaveral
 
 const locationElement = document.querySelector('.location');
 const weatherElement = document.querySelector('.weather');
+const windElement = document.querySelector('.wind');
 const marker = new mapboxgl.Marker();
 
-function getLocationInfo(coordinates) {
+function getLocationData(coordinates) {
   fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${coordinates.lng},${coordinates.lat}.json?types=place,locality,region,country&access_token=${mapsboxApiKey}`)
     .then((response) => response.json())
     .then((json) => {
-      console.log(json);
-
+      // Figure out what this place is called
       const target = locationElement.querySelector('h2');
 
       target.innerHTML = (json.features && json.features[0] && json.features[0].place_name) || '<i>unknown</i>';
@@ -34,35 +34,45 @@ function getWeatherData(coordinates) {
   fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lng}&units=metric&exclude=daily&appid=${weatherApiKey}`)
     .then((response) => response.json())
     .then((json) => {
-      console.log(json);
-
+      // LOCAL TIME
       // We need the time in UTC, in seconds sinds epoch, so we can calculate timezone offset
       const now = new Date();
-
       const localTimeSinceEpoch = now.getTime() + json.timezone_offset * 1000;
-      console.log(now.getTime());
-      console.log(localTimeSinceEpoch);
       const localTime = new Date(localTimeSinceEpoch);
 
       locationElement.querySelector('time').innerHTML = `${localTime.getUTCHours().toString().padStart(2, '0')}:${localTime.getUTCMinutes().toString().padStart(2, '0')}`;
 
+      // WEATHER ICON
       const imageTarget = weatherElement.querySelector('img');
       imageTarget.src = `http://openweathermap.org/img/wn/${json.current.weather[0].icon}@2x.png`;
       imageTarget.title = json.current.weather[0].description;
 
+      // TEMPERATURE
       weatherElement.querySelector('.weather__temperature b').innerHTML = json.current.temp.toFixed(1);
+
+      // WIND
+      windElement.querySelector('.wind__direction').style.transform = `rotate(${json.current.wind_deg}deg)`;
+      windElement.querySelector('.wind__speed b').innerHTML = json.current.wind_speed.toFixed(1);
     });
 }
 
 function handleLocationChange(coordinates) {
+  // Update the coordinates
   locationElement.querySelector('h1').innerHTML = `${coordinates.lng.toFixed(4)}, ${coordinates.lat.toFixed(4)}`;
+
+  // Call our APIs and update screen
   getWeatherData(coordinates);
-  getLocationInfo(coordinates);
+  getLocationData(coordinates);
+
+  // Place marker on new location
   marker
     .setLngLat(coordinates)
     .addTo(map);
 }
 
+/*
+ * MAPBOX SETUP & INIT
+ */
 mapboxgl.accessToken = mapsboxApiKey;
 
 const map = new mapboxgl.Map({
@@ -72,11 +82,14 @@ const map = new mapboxgl.Map({
   zoom: 5,
 });
 
+// Add zoom controls
 const nav = new mapboxgl.NavigationControl({ showCompass: false });
 map.addControl(nav, 'bottom-right');
 
+// Start by calling the APIs once on page load...
 handleLocationChange({ lng: startingPosition[0], lat: startingPosition[1] });
 
+// ... and then everytime you click somewhere
 map.on('click', (e) => {
   map.flyTo({ center: e.lngLat });
   handleLocationChange(e.lngLat);
